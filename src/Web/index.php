cb5503f792e72b203f4ed0d0ee5561f5a056b68c
mayhem-eng <feeduardogaviao@gmail.com>
2022-10-23 15:03:50 -0300

    Primeira versão dos arquivos do projeto cadastro de presencas

---
+++ src/Web/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<title>Cadastro Presenca</title>
+<head>
+    <style>
+        h1 {
+            color: green;
+        }
+    </style>
+</head>
+  
+<body>
+    <center>
+        <h1>Welcome To GFG</h1>
+  
+        <h2>
+            <?php 
+            echo "This is PHP code inside html"
+            ?>
+        </h2>
+		
+		<?php 
+		echo "<script>alert('JAVASCRIPT COM PHP');</script>";
+		?>
+    
+    </center>
+</body>
+  
+</html>
